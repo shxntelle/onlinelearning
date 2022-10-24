f1c341a9775148e9c8cb749acc309c4adea90eb1
Shantelle Lipana <@lipana.s@maristcollege.school.nz>
2022-10-24 14:00:32 +1300

    quiz

---
+++ quizquestions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Question 1 ?" ,
+    answer: "Option 3",
+    options: [
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Question 2 ?" ,
+    answer: "Option 1",
+    options: [
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4",
+    ]
+  },
+    {
+    numb: 3,
+    question: "Question 3 ?" ,
+    answer: "Option 3",
+    options: [
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Question 4 ?" ,
+    answer: "Option 1",
+    options: [
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Question 5 ?" ,
+    answer: "Option 5",
+    options: [
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
